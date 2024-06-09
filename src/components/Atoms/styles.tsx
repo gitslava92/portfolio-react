839b804aea5b3bcd21316a607fa33114befe2f20
gitslava92 <webslava92@gmail.com>
2024-06-09 20:59:57 +0300

    fixed scroll margin

--- src/components/Atoms/styles.tsx
+++ src/components/Atoms/styles.tsx
@@ -1,17 +1,22 @@
-import { Typography } from '@mui/material';
-import { Box as MUIBox, styled } from '@mui/system';
+import { Typography } from "@mui/material";
+import { Box as MUIBox, styled } from "@mui/system";
 
 
 export const Section = styled(MUIBox)(({ theme }) => ({
   padding: theme.spacing(5, 0),
+  position: "relative",
+  scrollMarginTop: 56,
+  [theme.breakpoints.up('md')]: {
+    scrollMarginTop: 64,
+  }
 }));
 
 export const Title = styled(Typography)(({ theme }) => ({
   color: theme.palette.text.primary,
-  fontWeight: 600,
+  fontWeight: 600
 }));
 
 export const ItemTitle = styled(Typography)(({ theme }) => ({
   color: theme.palette.text.primary,
-  fontWeight: 600,
+  fontWeight: 600
 }));
